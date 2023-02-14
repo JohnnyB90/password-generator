33768257c40d27d581656bcc84749fc93bf1ac2d
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-02-13 23:21:11 -0500

    Completed algorithms for randomizing the password, also completed functions to post password into the text area. Added a click to copy button to screen and completed functunality for it.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
-// Assignment Code
+// Variable assignments
 var generateBtn = document.querySelector("#generate");
+var textArea = document.querySelector("#password");
+var copyButton = document.querySelector("#copyButton");
+var textToCopy = document.querySelector(".text-to-copy");
 
 // Write password to the #password input
 function writePassword() {
@@ -8,128 +11,86 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Array of character possibilities
-let charactersUpper = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-let charactersLower = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+// An array of character possibilities
+let charactersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+let charactersLower = ["a","b","c","d","e","f","g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z",];
 let charactersSpecial = ["!", "@", "#", "$", "%", "^", "*"];
-let charactersNumbers = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-];
+let charactersNumbers = ["0","1","2","3","4","5","6","7","8","9","10",];
 
-// Add event listener to generate button
+// Added event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Added an event listener to copy button with function to copy password
+copyButton.addEventListener("click", function() {
+textToCopy.select();
+textToCopy.setSelectionRange(0, 99999); // For mobile devices
+navigator.clipboard.writeText(textToCopy.value);
+alert("Your password has been copied to the clipboard!!");
+});
 
 // Process anwsers to generate a password
 function generatePassword() {
   var confirmed = confirm("Would you like to generate a random password?");
-  var length =
-    "How many characters between 8-128 would you like your password?";
+  var length ="How many characters between 8-128 would you like your password?";
   var uppercase = "Would you like to use Uppercase?";
   var lowercase = "Would you like to use Lowercase?";
   var numbers = "Would you like to use numbers?";
   var special = "Would you like to use special characters?";
 
+// Confirmations to see what characters the users would like in their passwords.
+  const answers = {};
   if (confirmed === true) {
-    lengthAnwser = prompt(length);
-    // parseInt changes string to number
-    lengthAnwserNum = parseInt(lengthAnwser);
-    upperAnwser = confirm(uppercase);
-    lowerAnwser = confirm(lowercase);
-    numbersAnwser = confirm(numbers);
-    specialAnwser = confirm(special);
-    characterChoice();
+
+    answers['lengthAnwser'] = prompt(length);
+    answers['lengthAnwserNum'] = parseInt(answers['lengthAnwser']);
+    if ((answers['lengthAnwserNum'] < 8) || (answers['lengthAnwserNum'] > 128) || (!answers['lengthAnwserNum'])){ 
+      alert("Must be between 8-128 characters!!")
+    } else {
+    answers['upperAnwser'] = confirm(uppercase);
+    answers['lowerAnwser'] = confirm(lowercase);
+    answers['numbersAnwser'] = confirm(numbers);
+    answers['specialAnwser'] = confirm(special);
+};
+
+     const generatedPassword = characterChoice(answers);
+    return generatedPassword;
+  }
 }
+
+// Function to randomize the characters pushed into the creation array
+function randomize(creation) {
+  var randIndex = Math.floor(Math.random() * creation.length);
+  var randElement = creation[randIndex];
+  return randElement;
 }
+
 // Creation contains all characters being used based on users choice
 var creation = [];
 
-function randomize() {
-  for (i = 0; creation < lengthAnwserNum; i++) {
-    password = Math.floor(Math.random * creation.length);
-    password.toString();
-  }
-}
-
 // Pushes characters into creation array based on user choice
-function characterChoice() {
-  if (upperAnwser === true) {
+function characterChoice(answers) {
+  if (answers.upperAnwser === true) {
     for (i = 0; i < charactersUpper.length; i++)
-      creation.push(charactersUpper[i]);
+      creation.push(charactersUpper[i].toString());
   }
-  if (lowerAnwser === true) {
+  if (answers.lowerAnwser === true) {
     for (i = 0; i < charactersLower.length; i++)
-      creation.push(charactersLower[i]);
+      creation.push(charactersLower[i].toString());
   }
-  if (specialAnwser === true) {
+  if (answers.specialAnwser === true) {
     for (i = 0; i < charactersSpecial.length; i++)
-      creation.push(charactersSpecial[i]);
+      creation.push(charactersSpecial[i].toString());
   }
-  if (numbersAnwser === true) {
+  if (answers.numbersAnwser === true) {
     for (i = 0; i < charactersNumbers.length; i++)
-      creation.push(charactersNumbers[i]);
+      creation.push(charactersNumbers[i].toString());
   }
+  console.log(creation);
+  const password = [];
+  for(i =0; i<answers.lengthAnwserNum; i++){
+      const randomChar = randomize(creation);
+      password.push(randomChar);
+  }
+  return password.join('');
+  
 }
