e3d8b07decc1a17b4a50be8aa719b1d9688b7439
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-02-14 01:10:55 -0500

    Realized that the choices selection was not working correctly and the program would still put characters not chosen into the password. I added a fix for that so it would carefully check all possibilities for true or false

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,20 @@
 // Variable assignments
-var generateBtn = document.querySelector("#generate");
-var textArea = document.querySelector("#password");
-var copyButton = document.querySelector("#copyButton");
-var textToCopy = document.querySelector(".text-to-copy");
+const generateBtn = document.querySelector("#generate");
+const copyButton = document.querySelector("#copyButton");
+const textToCopy = document.querySelector(".text-to-copy");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 // An array of character possibilities
-let charactersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-let charactersLower = ["a","b","c","d","e","f","g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z",];
-let charactersSpecial = ["!", "@", "#", "$", "%", "^", "*"];
-let charactersNumbers = ["0","1","2","3","4","5","6","7","8","9","10",];
+let charactersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
+let charactersLower = ["a","b","c","d","e","f","g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z",]
+let charactersSpecial = ["!", "@", "#", "$", "%", "^", "*"]
+let charactersNumbers = ["0","1","2","3","4","5","6","7","8","9"]
 
 // Added event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -30,26 +29,24 @@ alert("Your password has been copied to the clipboard!!");
 
 // Process anwsers to generate a password
 function generatePassword() {
-  var confirmed = confirm("Would you like to generate a random password?");
-  var length ="How many characters between 8-128 would you like your password?";
-  var uppercase = "Would you like to use Uppercase?";
-  var lowercase = "Would you like to use Lowercase?";
-  var numbers = "Would you like to use numbers?";
-  var special = "Would you like to use special characters?";
-
+  const confirmed = confirm("Would you like to generate a random password?");
+  const length ="How many characters between 8-128 would you like your password?";
+  const uppercase = "Would you like to use Uppercase?";
+  const lowercase = "Would you like to use Lowercase?";
+  const numbers = "Would you like to use numbers?";
+  const special = "Would you like to use special characters?const
 // Confirmations to see what characters the users would like in their passwords.
   const answers = {};
   if (confirmed === true) {
-
-    answers['lengthAnwser'] = prompt(length);
-    answers['lengthAnwserNum'] = parseInt(answers['lengthAnwser']);
-    if ((answers['lengthAnwserNum'] < 8) || (answers['lengthAnwserNum'] > 128) || (!answers['lengthAnwserNum'])){ 
-      alert("Must be between 8-128 characters!!")
+    answers.lengthAnswer = prompt(length);
+    answers.lengthAnswerNum = parseInt(answers['lengthAnswer']);
+    if (!(answers.lengthAnswerNum >= 8 && answers.lengthAnswerNum <= 128))
+      {alert("Must be between 8-128 characters!!")
     } else {
-    answers['upperAnwser'] = confirm(uppercase);
-    answers['lowerAnwser'] = confirm(lowercase);
-    answers['numbersAnwser'] = confirm(numbers);
-    answers['specialAnwser'] = confirm(special);
+    answers['upperAnswer'] = confirm(uppercase);
+    answers['lowerAnswer'] = confirm(lowercase);
+    answers['numbersAnswer'] = confirm(numbers);
+    answers['specialAnswer'] = confirm(special);
 };
 
      const generatedPassword = characterChoice(answers);
@@ -59,38 +56,68 @@ function generatePassword() {
 
 // Function to randomize the characters pushed into the creation array
 function randomize(creation) {
-  var randIndex = Math.floor(Math.random() * creation.length);
-  var randElement = creation[randIndex];
+  let randIndex = Math.floor(Math.random() * creation.length);
+  let randElement = creation[randIndex];
   return randElement;
 }
 
-// Creation contains all characters being used based on users choice
-var creation = [];
 
 // Pushes characters into creation array based on user choice
+// I google foo'd this to solve my issues with not being able to properly select/exclude characters. I had to find a way to effectively evaluate all
+//potential options of true for the character choices.
+// This function evaluates the choices of true between each set of character arrays and fills an empty array called selectedCharacters based on that.
 function characterChoice(answers) {
-  if (answers.upperAnwser === true) {
-    for (i = 0; i < charactersUpper.length; i++)
-      creation.push(charactersUpper[i].toString());
+  const characterOptions = [
+    { characters: charactersUpper, enabled: answers.upperAnswer },
+    { characters: charactersLower, enabled: answers.lowerAnswer },
+    { characters: charactersSpecial, enabled: answers.specialAnswer },
+    { characters: charactersNumbers, enabled: answers.numbersAnswer }
+  ];
+
+  const selectedCharacters = characterOptions.filter(option => option.enabled)
+    .flatMap(option => option.characters);
+
+  if (selectedCharacters.length === 0) {
+    alert("You must select at least one type of character to include in your password!! Please hit generate password to try again.");
+    return;
   }
-  if (answers.lowerAnwser === true) {
-    for (i = 0; i < charactersLower.length; i++)
-      creation.push(charactersLower[i].toString());
-  }
-  if (answers.specialAnwser === true) {
-    for (i = 0; i < charactersSpecial.length; i++)
-      creation.push(charactersSpecial[i].toString());
-  }
-  if (answers.numbersAnwser === true) {
-    for (i = 0; i < charactersNumbers.length; i++)
-      creation.push(charactersNumbers[i].toString());
-  }
-  console.log(creation);
-  const password = [];
-  for(i =0; i<answers.lengthAnwserNum; i++){
-      const randomChar = randomize(creation);
-      password.push(randomChar);
-  }
-  return password.join('');
-  
+
+  const password = Array.from({length: answers.lengthAnswerNum}, () => randomize(selectedCharacters)).join('');
+  return password;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
